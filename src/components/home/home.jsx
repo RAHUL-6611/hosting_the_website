a957a82006a38aa9d0af9b57387cc6af0e7f4c65
swetha Baskaran <swethasambothi@gmail.com>
2022-01-30 19:56:48 +0530

    small change

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import CarouselHold from "../home/carousel_hold";
 import CardHold from "../home/cards/card_hold";
-import AboutUs from "./About Us/aboutus";
+// import AboutUs from "./About Us/aboutus";
 
 
 export default function Home() {
