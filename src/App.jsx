b4ccfc2cb8c058cc1947e68736babf1e2cb74714
rahul <rahulparmar7654321@gmail.com>
2022-01-31 18:57:05 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from './components/home/home'
 import './App.css'
 import Footer from './components/footer/footer'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <div className="App">
          {/* include nav header here */}
+         <Header />
          <Home />
          <Footer />
     </div>
---
+++ src/components/header/NavbarComponent.jsx
@@ -0,0 +1,171 @@
+import React,{useState} from 'react';
+import Navbar from "reactjs-navbar";
+// import logo from "../assets/logo.png";
+// import {Loader} from "react-loader-spinner";
+import {
+  faUsers,
+  faBookOpen,
+  faGlobe,
+  faChartPie,
+  faCogs /*...*/,
+  faAnchor,
+  faDizzy,
+  faAdjust,
+  faBell,
+  faGhost,
+  faFan,
+  faCarSide,
+    faJedi,
+    faWater,
+    faKey
+} from "@fortawesome/free-solid-svg-icons";
+ 
+import "reactjs-navbar/dist/index.css";
+import "./.css"
+
+const NavbarComponent = () => {
+
+    // const [isLoading, setIsLoading] = useState(false);
+  return (
+    <Navbar
+        // logo={logo}
+        // loader={<Loader type="Puff" color="#D85B5B" height={25} width={25} />}
+        // isLoading={isLoading}
+        className="fixed_navbar"
+        helpCallback={() => {
+          // alert("I need help... and coffee...");
+        }}
+        menuItems={[
+          {
+            title: "Academics",
+            icon: faUsers,
+            isAuth: true,
+            onClick: () => {
+              // What you want to do...
+              alert("Its coffee time...");
+            },
+          },
+          {
+            title: "Admissions",
+            icon: faBookOpen,
+            isAuth: true,
+            // isAuth: () => {
+            //   // Claim authorization logic...
+            //   return false;
+            // },
+          },
+          {
+            title: "For Students",
+            icon: faGlobe,
+            isAuth: true,
+          },
+          {
+            title: "For Faculty and Staff",
+            icon: faCogs,
+            isAuth: true,
+            subItems: [
+              {
+                title: "Faculty",
+                icon: faAnchor,
+                isAuth: true,
+                onClick: () => {
+                  // What you want to do...
+                  alert("Faculty ");
+                },
+              },
+              {
+                title: "Staff",
+                icon : faDizzy,
+                isAuth: true,
+                // subItems: [
+                //   { title: "Subitem 2-1", icon: faAdjust, isAuth: true },
+                //   {
+                //     title: "Subitem 2-2",
+                //     icon: faBell,
+                //     isAuth: true,
+                //     subItems: [
+                //       {
+                //         title: "Subitem 2-2-1",
+                //         icon: faGhost,
+                //         isAuth: true,
+                //         subItems: [
+                //           {
+                //             title: "Subitem 2-2-2-1",
+                //             icon: faFan,
+                //             isAuth: true,
+                //           },
+                          // {
+                          //   title: "Subitem 2-2-2-2",
+                          //   icon: faCarSide,
+                          //   isAuth: true,
+                          // },
+                          // {
+                          //   title: "Subitem 2-2-2-3",
+                          //   icon: faJedi,
+                          //   isAuth: true,
+                          // },
+                        //   {
+                        //     title: "Subitem 2-2-2-4",
+                        //     icon: faLaughBeam,
+                        //     isAuth: true,
+                        //   },
+                        // ],
+                      // },
+                      // {
+                        // title: "Subitem 2-2-2",
+                        // icon: faKey,
+                        // isAuth: true,
+                      // },
+                    // ],
+                  // },
+                //   {
+                //     title: "Make request",
+                //     icon: faCheese,
+                //     isAuth: true,
+                    // onClick: () => {
+                    //   // What you want to do...
+                    //   setIsLoading(true , () =>
+                    //     setTimeout(() => {
+                    //       setIsLoading(false);
+                    //     }, 2000)
+                    //   );
+                    // },
+                //   },
+                // ],
+              },
+              {
+                title: "Subitem 3",
+                icon: faWater,
+                isAuth: () => {
+                  // Claim authorization logic...
+                  return false;
+                },
+              },
+            ],
+          },
+          {
+            title: "Research",
+            icon: faChartPie,
+            isAuth: true,
+          },
+          {
+            title: "Alumni",
+            icon: faChartPie,
+            isAuth: true,
+          },
+          {
+            title: "Resources",
+            icon: faChartPie,
+            isAuth: true,
+          },
+          {
+            title: "Administration",
+            icon: faChartPie,
+            isAuth: true,
+          },
+        ]}
+      />
+  );
+};
+
+export default NavbarComponent;
