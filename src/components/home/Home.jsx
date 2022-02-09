9432e27cc867fa1c4877e9bb21f5688c29202bf0
rahul <rahulparmar7654321@gmail.com>
2022-02-10 00:22:52 +0530

    sw

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 // import CarouselHold from "./CarouselHold";
 // import StudentCount from "./student_Count/Count";
-import CampusLifeOne from "./campus_life/Campus_life1";
-import AboutComp from "./AboutPTU/about";
-import NewsPTU from "./news/NewsPTU";
+// import CampusLifeOne from "./campus_life/Campus_life1";
+// import AboutComp from "./AboutPTU/about";
+// import NewsPTU from "./news/NewsPTU";
 import Header from "../head_sec/Header";
 
 
@@ -11,11 +11,11 @@ export default function Home() {
     return (
         <div>
             <Header/>
-           <AboutComp />
-           <NewsPTU />
+           {/* <AboutComp /> */}
+           {/* <NewsPTU /> */}
            {/* <CarouselHold /> */}
            {/* <StudentCount /> */}
-           <CampusLifeOne />
+           {/* <CampusLifeOne /> */}
         </div>
     )
 }
