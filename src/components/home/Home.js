c083d81cfcd2d65595ed6f9e4f1ae7bd470c3558
rahul <rahulparmar7654321@gmail.com>
2022-02-09 23:51:25 +0530

    sw

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,6 @@
 import React from "react";
-
 import CarouselHold from "./carousel_hold";
-// import CardHold from "../home/cards/card_hold";
-// import AboutUs from "./About Us/aboutus";
 import StudentCount from "./student_Count/count";
-// import NewsBlock from "./newsEvents/newsBlock";
 import CampusLifeOne from "./campus_life/campus_life1";
 import AboutComp from "./AboutPTU/about";
 import NewsPTU from "./news/NewsPTU";
