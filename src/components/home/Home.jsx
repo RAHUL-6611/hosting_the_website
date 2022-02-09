bace7b1172a60d2547c7ae2c7d874647aa804116
rahul <rahulparmar7654321@gmail.com>
2022-02-10 00:20:58 +0530

    sw

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import CarouselHold from "./CarouselHold";
-import StudentCount from "./student_Count/Count";
+// import StudentCount from "./student_Count/Count";
 import CampusLifeOne from "./campus_life/Campus_life1";
 import AboutComp from "./AboutPTU/about";
 import NewsPTU from "./news/NewsPTU";
