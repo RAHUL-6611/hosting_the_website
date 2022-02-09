1acdc6c4a5263644345e665a31b17f5321d9f0dc
rahul <rahulparmar7654321@gmail.com>
2022-02-10 00:10:31 +0530

    sw

--- src/components/home/carousel_hold.jsx
+++ src/components/home/carousel_hold.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import Carousel from "./carousel";
+import Carousel from "./Carousel";
 
 export default function CarouselHold(){
     return (
         <>
-        <div className="div" id="">
+        <div className="div">
            <h1 id="">Departments</h1>
             <div>
                <Carousel />
             </div>
         </div>
         
-        {/* <div className="div" id="">
-          <h1 id="">Latest Research</h1>
-            <div>
-               <Carousel />
-            </div>
-        </div> */}
+  
        </>
     )
 }
